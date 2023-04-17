6673d23b95c9cb2368bb9cc5c3b560839fdf34ba
nimaaskari <nimaa.dev@gmail.com>
2023-04-17 15:06:42 +0330

    add components to the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,35 @@
 import React from 'react';
+import { useState } from 'react';
+import { createStage } from './gameHelpers';
 // Styles
 import { StyledTetrisWrapper, StyledTetris } from './App.styles';
+import Display from './components/Display/Display';
+import StartButton from './components/StartButton/StartButton';
+import Stage from './components/Stage/Stage';
 
 const App: React.FC = () => {
+  const [dropTime, setDropTime] = React.useState<null | number>(null);
+  const [gameOver, setGameOver] = React.useState(true)
+
   return (
     <StyledTetrisWrapper role='button' tabIndex={0}>
-      <StyledTetris>Start here!</StyledTetris>
+      <StyledTetris>
+        <div className="display">
+          {gameOver ? (
+            <>
+              <Display gameOver={gameOver} text="Game Over!" />
+              <StartButton callback={() => null} />
+            </>
+          ) : (
+            <>
+              <Display text={`Score:`} />
+              <Display text={`Rows:`} />
+              <Display text={`Level:`} />
+            </>
+          )}
+        </div>
+        <Stage stage={createStage} />
+      </StyledTetris>
     </StyledTetrisWrapper>
   );
 };
--- src/components/StartButton/StartButton.tsx
+++ src/components/StartButton/StartButton.tsx
@@ -5,8 +5,8 @@ type Props = {
   callback: () => void;
 }
 
-const StartButton: React.FC<Props> = ({ callback }) => {
+const StartButton: React.FC<Props> = ({ callback }) => (
   <StyledStartButton onClick={callback} >Start</StyledStartButton>
-}
+)
 
 export default StartButton;
